d228d2c82e828f1ae2a3b624e68b736367ff51cc
noah.guerin <noahguerin20@gmail.com>
2026-03-13 20:57:28 +0100

    first commit

---
+++ src/main/java/com/livesportsstreaks/LiveSportsStreaksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.livesportsstreaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveSportsStreaksBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveSportsStreaksBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livesportsstreaks/LiveSportsStreaksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livesportsstreaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSportsStreaksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
